74d8d34c3d43eef47aba7fcdf922389d87b82450
chaitanya <chaitanyaharshan.official@gmail.com>
2024-07-30 16:59:56 +0530

    changed file name

--- arrays/easy/removeDuplicates_sorted.java
+++ arrays/easy/removeDuplicates_sorted.java
@@ -2,7 +2,7 @@ package arrays.easy;
 
 import java.util.Arrays;
 
-public class removeDuplicates {
+public class removeDuplicates_sorted {
 
     public static void main(String[] args) {
         int[] arr = {1,2,3,3,3,5,6,6,7,7};
