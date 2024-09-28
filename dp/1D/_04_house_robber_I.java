7ce451aa235f7b6a526cc7321774a2aba56557eb
chaitanya <chaitanyaharshan.official@gmail.com>
2024-09-28 11:48:54 +0530

    1d dp striver is completed

---
+++ dp/1D/_04_house_robber_I.java
@@ -0,0 +1,59 @@
+/**
+ * _04_house_robber_I
+ */
+public class _04_house_robber_I {
+
+    public int rob(int[] nums) {
+        int nxt = 0, nxtNxt = 0;
+        int cur = 0;
+        for (int i=nums.length-1; i>=0; i--) {
+            cur = Math.max(nums[i]+nxtNxt, nxt);
+            nxtNxt = nxt;
+            nxt = cur;
+        }
+        return cur;
+
+
+        // if (nums.length == 1) return nums[0];
+        // if (nums.length == 2) return Math.max(nums[0], nums[1]);
+        // int prev = nums[0];
+        // int current = Math.max(prev, nums[1]);
+
+        // int prev = 0, current = 0;
+        // for (int i = 0; i<nums.length; i++) {
+        //     int temp = current;
+        //     current = Math.max(nums[i] + prev, temp);
+        //     prev = temp;
+        // }
+        // return current;
+    }
+}
+
+/*
+ * 198. House Robber
+ * https://leetcode.com/problems/house-robber/description/
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+Example 2:
+
+Input: nums = [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+Total amount you can rob = 2 + 9 + 1 = 12.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 400
+ */
---
+++ dp/1D/_05_house_robber_II.java
@@ -0,0 +1,53 @@
+package dp.1D;
+
+public class _05_house_robber_II {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        return Math.max(robbing(nums,n-1,1), robbing(nums, n-2,0));
+    }
+
+    static int robbing(int[] nums, int start, int end) {
+        int nxt = 0, nxtNxt = 0;
+        int cur = 0;
+        for (int i=start; i>= end; i--) {
+            cur = Math.max(nums[i]+nxtNxt, nxt);
+            nxtNxt = nxt;
+            nxt = cur;
+        }
+        return cur;
+    }
+}
+
+/*
+ * 213. House Robber II
+ * https://leetcode.com/problems/house-robber-ii/description/
+
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have a security system connected, and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,2]
+Output: 3
+Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2), because they are adjacent houses.
+Example 2:
+
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 3
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 1000
+ */
