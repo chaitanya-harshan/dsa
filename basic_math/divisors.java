d1a4e03e1883983be954d46d4641982f2720d886
chaitanya <chaitanyaharshan.official@gmail.com>
2024-12-04 16:32:37 +0530

    divisors sum

--- basic_math/divisors.java
+++ basic_math/divisors.java
@@ -1,27 +1,54 @@
-import java.util.ArrayList;
 
 public class divisors {
-    public static void main(String[] args) {
-        
-        ArrayList<Integer> divisors = printDivisors(44);
-        System.out.println(divisors);
-    }
-
-    static ArrayList<Integer> printDivisors(int N) {
-
-        int root = (int)Math.sqrt(N);
-        ArrayList<Integer> divisors = new ArrayList<>();
     
-        for (int i = 1; i <= root; i++) {
-            if (N % i == 0)  divisors.add(i);
+    public static void main(String[] args) {
+        System.out.println(sumOfDivisors(6));
+    }
+    
+    public static int sumOfDivisors(int n) {
+        // code here
+        int sum = 0;
+        for (int i=1; i<=n; i++) {
+            System.out.println(divisors(i));
+            sum += divisors(i);
         }
-        for (int i = divisors.size()-1; i >= 0; i--) {
-
-            int comp_Divisor = N / divisors.get(i);
-            if (divisors.get(i) != comp_Divisor) {
-                divisors.add(comp_Divisor);
+        return sum;
+    }
+    
+    public static int divisors(int num) {
+        int sum = 0;
+        int sqrt = (int)Math.sqrt(num);
+        for (int n = 1; n <= sqrt; n++) {
+            if (num%n == 0) {
+                sum += n;
+                int comp_div = num/n;
+                if (comp_div != n) sum += comp_div;
             }
         }
-        return divisors;
+        return sum;
     }
+    
+    // public static void main(String[] args) {
+        
+    //     ArrayList<Integer> divisors = printDivisors(44);
+    //     System.out.println(divisors);
+    // }
+
+    // static ArrayList<Integer> printDivisors(int N) {
+
+    //     int root = (int)Math.sqrt(N);
+    //     ArrayList<Integer> divisors = new ArrayList<>();
+    
+    //     for (int i = 1; i <= root; i++) {
+    //         if (N % i == 0)  divisors.add(i);
+    //     }
+    //     for (int i = divisors.size()-1; i >= 0; i--) {
+
+    //         int comp_Divisor = N / divisors.get(i);
+    //         if (divisors.get(i) != comp_Divisor) {
+    //             divisors.add(comp_Divisor);
+    //         }
+    //     }
+    //     return divisors;
+    // }
 }
--- dp/1D/_04_house_robber_I.java
+++ dp/1D/_04_house_robber_I.java
@@ -18,7 +18,6 @@ public class _04_house_robber_I {
         // if (nums.length == 2) return Math.max(nums[0], nums[1]);
         // int prev = nums[0];
         // int current = Math.max(prev, nums[1]);
-
         // int prev = 0, current = 0;
         // for (int i = 0; i<nums.length; i++) {
         //     int temp = current;
@@ -32,9 +31,13 @@ public class _04_house_robber_I {
 /*
  * 198. House Robber
  * https://leetcode.com/problems/house-robber/description/
-You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+You are a professional robber planning to rob houses along a street. 
+Each house has a certain amount of money stashed, the only constraint stopping you 
+from robbing each of them is that adjacent houses have security systems connected and 
+it will automatically contact the police if two adjacent houses were broken into on the same night.
 
-Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+Given an integer array nums representing the amount of money of each house, 
+return the maximum amount of money you can rob tonight without alerting the police.
 
  
 
