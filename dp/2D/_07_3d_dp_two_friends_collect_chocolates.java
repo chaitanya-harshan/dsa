b76688a0eb4795214fb6f73f8da8979f97443794
chaitanya <chaitanyaharshan.official@gmail.com>
2024-12-07 20:07:21 +0530

    wrote explanation for max chocolates collection in 2d dp 7th prob

--- dp/2D/_07_3d_dp_two_friends_collect_chocolates.java
+++ dp/2D/_07_3d_dp_two_friends_collect_chocolates.java
@@ -8,6 +8,8 @@ public class _07_3d_dp_two_friends_collect_chocolates {
         System.out.println(maximumChocolates(4, 3, grid));
     }
 
+    // Here MEMOIZATION is better than TABULATION becasue u dont need to calculate all sub problems
+
     // tabulation  (below there is code for memoization)
     public static int solve(int m, int n, int[][] grid) {
         int[][] dp = new int[n][n];
@@ -17,7 +19,6 @@ public class _07_3d_dp_two_friends_collect_chocolates {
                 else dp[i][j] = grid[m-1][i] + grid[m-1][j];
             }
         }
-
         for (int i = m-2; i >= 0; i--) {
 
             int[][] dpNew = new int[n][n];
@@ -28,16 +29,16 @@ public class _07_3d_dp_two_friends_collect_chocolates {
                     if (j1 == j2) sum = grid[i][j1];
                     else sum = grid[i][j1] + grid[i][j2];
 
-                    int bottom, maxBottom = Integer.MIN_VALUE;
+                    int max = Integer.MIN_VALUE;
                     for (int di = -1; di <= 1; di++) {
                         for (int dj = -1; dj <= 1; dj++) {
-
-                            if (j1+di < 0 || j2+dj < 0 || j1+di >= n || j2+dj >= n) bottom = Integer.MIN_VALUE;
-                            else bottom = dp[j1+di][j2+dj];
-                            maxBottom = Math.max(bottom, maxBottom);
+                            int ans;
+                            if (j1+di < 0 || j2+dj < 0 || j1+di >= n || j2+dj >= n) ans = Integer.MIN_VALUE;
+                            else ans = dp[j1+di][j2+dj];
+                            max = Math.max(ans, max);
                         }
                     }
-                    dpNew[j1][j2] = sum + maxBottom;    
+                    dpNew[j1][j2] = sum + max;    
                 }
             }
             dp = dpNew;
@@ -72,7 +73,7 @@ public class _07_3d_dp_two_friends_collect_chocolates {
 			for (int db = -1; db <= 1; db++) {
 				int ans;
 				if (j1 == j2)
-					ans = grid[i][j1] + backtrack(i+1, j1+da, j2+db, grid, dp, n, m);
+					 ans = grid[i][j1] + backtrack(i+1, j1+da, j2+db, grid, dp, n, m);
 				else ans = grid[i][j1] + grid[i][j2] + backtrack(i+1, j1+da, j2+db, grid, dp, n, m);
 				
 				max = Math.max(max, ans);
@@ -82,9 +83,37 @@ public class _07_3d_dp_two_friends_collect_chocolates {
 	}
 }
 
+// ************************************************* IF YOUR'E BREAKING YOUR HEAD*******************
+// if eg: nx3x3
+// nx3x3  becasue no. of directions each can take.
+// for some i let's say 'A' is in a column and 'B' is in b column then a slice has all the postions
+// of a & b such that (a-1,a,a+1) x (b-1,b,b+1) => 3x3
+// for a dp[i][a][b] it'll tell the max value u can get if you go from (i-1)
+// to [i][a][b]
+// _______IMPORTANT TO READ AFTER YOU READ THE ABOVE____
+// but if we have like 10 columns then 'A' can be 3rd and 'B' can in 7 then u need [2,3,4] & [6,7,8]
+// therfore we need to store all the instance/paths of [0-cols] x [0-cols] for every slice or 'i' becasue
+//  some paths need some and other paths needs other cols which might not overlap in the columns required
+
+// _______ BOTH ARE MOVING CONTINOUS___________
+// you can't think that first u will find the max of A then u will find B because while finding max from a 'i' 
+// we can again get a ja=jb which means sometimes u should skip local maxima ans choose diff path to get the global maxima.  
+// If you're thinking if they both are in the same j then from then onwards they both will have same 
+// max values from there but NO. the algo will try all the 3x3 cases from that 'i' and then from then 
+// the path where they take all the same directions 
+// (both have same path/matrix_instances => nxt direction -> {j1+da == j2+db}) will not be max 
+// and other paths will have max path as in this path it's always one of the robot gets 0 chocolates.
+
+// TO BE CLEAR: Here path means the instance of the 3D matrix wich means combination of both paths.
+// The algo doesnt care about who gets how much, it only cares about the total max sum so therefore when both
+// start from same j then there's no argument of what if [t=takes, nt = not takes] once At,Bnt : Ant,bt etc to equalize
+// ****************************************************************************************************
+
+
 
 /*
- * You are given an n rows and m cols matrix grid representing a field of chocolates where grid[i][j] represents the number of chocolates that you can collect from the (i, j) cell.
+ * You are given an n rows and m cols matrix grid representing a field of chocolates where grid[i][j] represents 
+ * the number of chocolates that you can collect from the (i, j) cell.
 
 You have two robots that can collect chocolates for you:
 
